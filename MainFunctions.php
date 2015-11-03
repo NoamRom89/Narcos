0b09ac0d44b2c18f5246b9a53ecef41b0486ca7a
Noam Rom <rom.noam89@gmail.com>
2015-11-03 16:56:57 +0200

    3 commit

--- MainFunctions.php
+++ MainFunctions.php
@@ -13,7 +13,7 @@
 	// Loads database from file.
 	function loadDB(){
 		$json_db = file_get_contents("DataBase.json");
-		$json_db = str_replace("null", "\"null\"", $json_db);
+		$json_db = str_replace("null","\"null\"", $json_db);
 		return json_decode($json_db, true);
 	} // loadDB
 	
@@ -27,26 +27,29 @@
 	function initDB(){
 	
 		// Set "Stop List" words.
-		$stopList = array('a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as', 'at',
-					  'be', 'because', 'been', 'but', 'by',
-					  'can', 'cannot', 'could',
-					  'dear', 'did', 'do', 'does',
-					  'either', 'else', 'ever', 'every',
-					  'for', 'from',
-					  'get', 'got',
-					  'had', 'has', 'have', 'he', 'her', 'hers', 'him', 'his', 'how', 'however',
-					  'i', 'if', 'in', 'into', 'is', 'it', 'its',
-					  'just',
-					  'least', 'let', 'like', 'likely',
-					  'may', 'me', 'might', 'most', 'must', 'my',
-					  'neither', 'no', 'nor', 'not',
-					  'of', 'off', 'often', 'on', 'only', 'or', 'other', 'our', 'own',
-					  'rather',
-					  'said', 'say', 'says', 'she', 'should', 'since', 'so', 'some',
-					  'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas',
-					  'us',
-					  'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'would',
-					  'yet', 'you', 'your');
+		$stopList = array(
+			'a', 'able', 'about', 'across', 'after', 'all', 'almost',
+			'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as', 'at',
+			'be', 'because', 'been', 'but', 'by',
+			'can', 'cannot', 'could',
+			'dear', 'did', 'do', 'does',
+			'either', 'else', 'ever', 'every',
+			'for', 'from',
+			'get', 'got',
+			'had', 'has', 'have', 'he', 'her', 'hers', 'him', 'his', 'how', 'however',
+			'i', 'if', 'in', 'into', 'is', 'it', 'its',
+			'just',
+			'least', 'let', 'like', 'likely',
+			'may', 'me', 'might', 'most', 'must', 'my',
+			'neither', 'no', 'nor', 'not',
+			'of', 'off', 'often', 'on', 'only', 'or', 'other', 'our', 'own',
+			'rather',
+			'said', 'say', 'says', 'she', 'should', 'since', 'so', 'some',
+			'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas',
+			'us',
+			'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with','would',
+			'yet', 'you', 'your'
+		);
 
 		// If DB exists - Load it. If not, create a new DB.
 		if (existsDB()){
